ba0d39e9fa49027661c0d48def0d5ce87e922843
degemarps <degemarpereira@gmail.com>
2022-01-03 17:38:52 -0300

    Projeto solar system

---
+++ project-solar-system/src/components/PlanetCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+
+    return (
+      <div data-testid="planet-card">
+        <h3 data-testid="planet-name">{planetName}</h3>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: propTypes.string.isRequired,
+  planetImage: propTypes.string.isRequired,
+};
+
+export default PlanetCard;
