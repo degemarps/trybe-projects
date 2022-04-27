f802279b348cd3face9107adeeb6664b20819d5d
degemarps <degemarpereira@gmail.com>
2022-04-26 22:21:52 -0300

    Mudança de diretórios

---
+++ Front-End/project-frontend-online-store/build/precache-manifest.235fa6afa0de475e912afab79cdc5f3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e374221059539c98bb7380fea31a4bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34aaee81e4198cfb536a",
+    "url": "/static/css/main.7775c886.chunk.css"
+  },
+  {
+    "revision": "f9776e6abb7080ac2f43",
+    "url": "/static/js/2.2fd964f7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2fd964f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34aaee81e4198cfb536a",
+    "url": "/static/js/main.5c53bc6b.chunk.js"
+  },
+  {
+    "revision": "837674402e425cca2f8d",
+    "url": "/static/js/runtime-main.6839dbe2.js"
+  },
+  {
+    "revision": "e7322a6673613ab13604fadda3d20e56",
+    "url": "/static/media/frontendOS_font.e7322a66.ttf"
+  },
+  {
+    "revision": "74f2e4ab4dd563f790f1057ddc95cc79",
+    "url": "/static/media/logo_frontendOnlineStore.74f2e4ab.svg"
+  }
+]);
